a3397244f71fc905baace18302491d2c44b52bb6
DmytroKrylov <120196125+DmytroKrylov@users.noreply.github.com>
2023-07-07 11:53:54 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Component } from 'react';
-import Statistics from './components/Feedback/Statistics/Statistics';
-import FeedbackOptions from './components/Feedback/FeedbackOptions/FeedbackOptions';
-import Section from './components/Feedback/Section/Section';
-import Notification from './components/Feedback/Notification/Notification';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
 
 class App extends Component {
   state = {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
 import './index.css';
-
-// export const getRandomColor = () => {
-//   const letters = '0123456789ABCDEF';
-//   let color = '#';
-//   for (let i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// };
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
